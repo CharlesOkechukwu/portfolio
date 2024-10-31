358be992896f6ef0b3c76974303a4d059522d704
CharlesOkechukwu <realcharlieok@gmail>
2024-10-31 23:21:48 +0100

    add animation

--- index.js
+++ index.js
@@ -5,4 +5,38 @@ document.addEventListener('DOMContentLoaded', function() {
     menu.addEventListener('click', function() {
         nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
     });
+
+    var edit = document.getElementById('editable');
+    var inputs = ['a software engineer', 'from Nigeria', 'passionate about web development', 
+        'efficient with python and nodejs',
+        'skilled in HTML, CSS, jQuery, javaScript, React', 
+        'glad you are here!', 'excited to work with you!'];
+    var i = 0;
+    var charIndex = 0;
+    var typeInterval;
+    function changeText() {
+        if (charIndex < inputs[i].length) {
+            edit.innerHTML += inputs[i][charIndex];
+            console.log(i);
+            charIndex++;
+        } else {
+            clearInterval(typeInterval);
+            setTimeout(deleteText, 1000);
+        }
+    }
+
+    function deleteText() {
+        if (charIndex > 0) {
+            edit.innerHTML = edit.innerHTML.slice(0, --charIndex);
+            console.log(charIndex);
+        } else {
+            i = (i + 1) % inputs.length;
+            clearInterval(typeInterval);
+            setTimeout(() => {
+                charIndex = 0;
+                typeInterval = setInterval(changeText, 100);
+            }, 1000);
+        }
+    }
+    typeInterval = setInterval(changeText, 100);
 });
